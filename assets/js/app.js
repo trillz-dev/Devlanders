70f5cdaba3702a9a6ed4d2b633cd714f1abc3e91
trillz-dev <timorazzy@gmail.com>
2020-10-16 15:51:54 +0100

    project update

---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
+const textArray = ['Amazon Severless.', 'Java.', 'PHP.', 'Symphony.', 'Node.js.', 'React.', 'Vue.js.', 'Angular.' ];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1000;
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains('typing')) {
+            cursorSpan.classList.add('typing')
+        };
+
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay)
+    } else {
+        cursorSpan.classList.remove('typing');
+
+        setTimeout(erase, newTextDelay)
+        
+    }
+}
+
+function erase () {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains('typing')) {
+            cursorSpan.classList.add('typing')
+        };
+
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursorSpan.classList.remove('typing');
+
+        textArrayIndex ++;
+        if (textArrayIndex >= textArray.length) {
+            textArrayIndex = 0;
+        };
+        setTimeout(type, typingDelay + 1100)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (textArray.length) {
+    setTimeout(type, newTextDelay)
+    }
+});
